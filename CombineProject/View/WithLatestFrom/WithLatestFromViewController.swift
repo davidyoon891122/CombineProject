43f59848a8004f5b6c01851d4036a94b0a9150b3
David Yoon <davidyoon@hanwha.com>
2024-04-22 15:59:11 +0900

    Add a WithLatestFromVC
    * Added a WithLatestFromVC
    * Bound the viewModel

--- CombineProject/View/WithLatestFrom/WithLatestFromViewController.swift
+++ CombineProject/View/WithLatestFrom/WithLatestFromViewController.swift
@@ -6,16 +6,84 @@
 //
 
 import UIKit
-import SnapKit
 import Combine
+import CombineCocoa
+import SnapKit
 
 final class WithLatestFromViewController: UIViewController {
     
     private let viewDidLoadPublisher: PassthroughSubject<Void, Never> = .init()
-    
     private var cancellables: Set<AnyCancellable> = []
     
+    // UI
+    private lazy var displayLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 22)
+        
+        label.layer.borderColor = UIColor.gray.cgColor
+        label.layer.borderWidth = 1.0
+        label.layer.cornerRadius = 4
+        
+        return label
+    }()
+    
+    private lazy var numberButton: UIButton = {
+        var configure = UIButton.Configuration.plain()
+        configure.title = "Number"
+        
+        let button = UIButton(configuration: configure)
+        button.backgroundColor = .black
+        button.tintColor = .white
+        button.layer.cornerRadius = 4
+        
+        return button
+    }()
+    
+    private lazy var stringButton: UIButton = {
+        var configure = UIButton.Configuration.plain()
+        configure.title = "String"
+        
+        let button = UIButton(configuration: configure)
+        button.backgroundColor = .black
+        button.tintColor = .white
+        button.layer.cornerRadius = 4
+        
+        return button
+    }()
+    
+    
+    private lazy var buttonHStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 8
+        
+        [
+            self.numberButton,
+            self.stringButton
+        ]
+            .forEach {
+                stackView.addArrangedSubview($0)
+            }
+        
+        return stackView
+    }()
+    
+    private lazy var resetButton: UIButton = {
+        var configure = UIButton.Configuration.plain()
+        configure.title = "RESET"
+        
+        let button = UIButton(configuration: configure)
+        button.backgroundColor = .red
+        button.tintColor = .white
+        button.layer.cornerRadius = 4
+        
+        return button
+    }()
+    
+    
     private let viewModel: WithLatestFromViewModel
+
     
     init(viewModel: WithLatestFromViewModel) {
         self.viewModel = viewModel
@@ -33,24 +101,97 @@ final class WithLatestFromViewController: UIViewController {
         self.viewDidLoadPublisher.send()
     }
     
+    deinit {
+        print("DEINIT \(type(of: self))")
+    }
+    
 }
 
 private extension WithLatestFromViewController {
     
     func setupViews() {
         self.view.backgroundColor = .systemBackground
+        [
+            self.displayLabel,
+            self.buttonHStackView,
+            self.resetButton
+        ]
+            .forEach {
+                self.view.addSubview($0)
+            }
+        
+        self.displayLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(16.0)
+            $0.height.equalTo(300.0)
+        }
+        
+        self.buttonHStackView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(16.0)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(50.0)
+        }
+        
+        self.resetButton.snp.makeConstraints {
+            $0.top.equalTo(self.displayLabel.snp.bottom).offset(8)
+            $0.trailing.equalTo(self.displayLabel)
+            $0.height.equalTo(30)
+        }
+        
     }
     
     func bindViewModel() {
-        let outputs = self.viewModel.bind(.init(viewDidLoad: self.viewDidLoadPublisher.eraseToAnyPublisher()))
+        var number = 0
+        var string = ""
+        
+        let numberPublisher = self.numberButton.tapPublisher
+            .flatMap {  _ -> AnyPublisher<Int, Never> in
+                number += 1
+                return Just(number)
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+        
+        let stringPublisher = self.stringButton.tapPublisher
+            .flatMap { _ -> AnyPublisher<String, Never> in
+                string += "a"
+                return Just(string)
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+        
+        let outputs = self.viewModel.bind(.init(
+            viewDidLoad: self.viewDidLoadPublisher.eraseToAnyPublisher(),
+            numberButton: numberPublisher,
+            stringButton: stringPublisher,
+            resetButton: self.resetButton.tapPublisher
+        ))
         
         [
             outputs.title.assign(to: \.title, on: self.navigationItem),
-            outputs.events.sinkIgnored()
+            outputs.displayValue
+                .print("DisplayValue")
+                .assign(to: \.text, on: self.displayLabel),
+            outputs.events.sinkIgnored(),
+            outputs.reset.sink(receiveValue: { [weak self] _ in
+                number = 0
+                string = ""
+                self?.reset()
+            })
         ]
             .forEach {
                 self.cancellables.insert($0)
             }
     }
     
+    func reset() {
+        self.displayLabel.text = nil
+        self.cancellables = []
+        self.bindViewModel()
+    }
+    
+}
+
+#Preview {
+    WithLatestFromViewController(viewModel: WithLatestFromViewModel(navigator: WithLatestFromNavigator(navigationController: UINavigationController())))
 }
--- CombineProject/View/WithLatestFrom/WithLatestFromViewModel.swift
+++ CombineProject/View/WithLatestFrom/WithLatestFromViewModel.swift
@@ -12,11 +12,16 @@ struct WithLatestFromViewModel {
     
     struct Inputs {
         let viewDidLoad: AnyPublisher<Void, Never>
+        let numberButton: AnyPublisher<Int, Never>
+        let stringButton: AnyPublisher<String, Never>
+        let resetButton: AnyPublisher<Void, Never>
     }
     
     struct Outputs {
         let title: AnyPublisher<String?, Never>
+        let displayValue: AnyPublisher<String?, Never>
         let events: AnyPublisher<Void, Never>
+        let reset: AnyPublisher<Void, Never>
     }
     
     private let navigator: WithLatestFromNavigatorProtocol
@@ -32,18 +37,39 @@ extension WithLatestFromViewModel {
     func bind(_ inputs: Inputs) -> Outputs {
         
         let titlePublisher = PassthroughSubject<String?, Never>()
+        let displayValuePublisher = PassthroughSubject<String?, Never>()
+        let resetPublisher = PassthroughSubject<Void, Never>()
         
         let events = Publishers.MergeMany(
             inputs.viewDidLoad
                 .handleEvents(receiveOutput: {
                     titlePublisher.send("WithLatestFrom")
                 })
+                .map { _ in }
+                .eraseToAnyPublisher()
+            ,
+            inputs.numberButton
+                .withLatestFrom(inputs.stringButton) { ($0, $1) }
+                .handleEvents(receiveOutput: { (number, string) in
+                    displayValuePublisher.send("\(number) and \(string)")
+                })
+                .map { _ in }
+                .eraseToAnyPublisher(),
+            inputs.resetButton
+                .handleEvents(receiveOutput: {
+                    resetPublisher.send()
+                })
+                .map { _ in }
+                .eraseToAnyPublisher()
         )
+            .eraseToAnyPublisher()
         
         
         return .init(
             title: titlePublisher.eraseToAnyPublisher(),
-            events: events.eraseToAnyPublisher()
+            displayValue: displayValuePublisher.eraseToAnyPublisher(),
+            events: events.eraseToAnyPublisher(),
+            reset: resetPublisher.eraseToAnyPublisher()
         )
     }
     
